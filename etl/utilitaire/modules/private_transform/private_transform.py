6008c5ab3d05b93f9e8e9fb7d7d7d2756824b3f6
beatricedaniel <beatrice.yk.daniel@protonmail.com>
2023-02-16 11:09:16 +0000

    new file activites vs savoirfaire created

--- etl/utilitaire/modules/private_transform/private_transform.py
+++ etl/utilitaire/modules/private_transform/private_transform.py
@@ -5,7 +5,7 @@ import pandas as pd
 from .query_private_transform import *
 
 
-def transform_export(filepath_activites, filepath_personnes, database="database", verbose = True) :
+def transform_export(filepath_activites, filepath_personnes, filepath_activites_vs_savoirfaire, database="database", verbose = True) :
     """
     Création des fichiers activités.csv et personnes.csv à partir des fichiers .csv 
     présents dans data/input.
@@ -23,6 +23,8 @@ def transform_export(filepath_activites, filepath_personnes, database="database"
         os.remove(filepath_activites)
     if os.path.exists(filepath_personnes):
         os.remove(filepath_personnes)
+    if os.path.exists(filepath_activites_vs_savoirfaire):
+        os.remove(filepath_activites_vs_savoirfaire)
 
     # Transformation et export des données activités
     print(" ")
@@ -44,3 +46,14 @@ def transform_export(filepath_activites, filepath_personnes, database="database"
        chunk.to_csv(os.path.join(filepath_personnes), index = True, index_label = "INDEX", mode='a',sep=';',encoding='utf-8')
 
     print(" --- select_personnes_sql : OK")
+
+    # transformation et export des données activites vs savoir-faire
+    print(" ")
+    print(" - - - Transformation et export des données activites vs savoir-faire...")
+
+    select_activites_vs_savoirfaire_sql = query_select_activites_vs_savoirfaire_sql()
+
+    for chunk in pd.read_sql_query(select_activites_vs_savoirfaire_sql, conn, chunksize=10000):
+       chunk.to_csv(os.path.join(filepath_activites_vs_savoirfaire), index = True, index_label = "INDEX", mode='a',sep=';',encoding='utf-8')
+
+    print(" --- select_activites_vs_savoirfaire_sql : OK")
